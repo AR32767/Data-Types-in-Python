79af78eb5c539732b8490c0bd511b9edd603aec5
AR32767 <arjun05.nair@gmail.com>
2025-05-14 18:27:36 -0400

    commit

---
+++ Data Types.py
@@ -0,0 +1,6 @@
+a = "Enter something here..."
+b = 10
+c = False
+d = 3.14
+print("Returning data types from variables")
+print(a, "->", type(a), b, "->", type(b), c,"->", type(c), d, "->", type(d))
