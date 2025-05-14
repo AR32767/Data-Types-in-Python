f6aa6ee2ce99530f8105eec2936a1f6aed57dc1f
AR32767 <arjun05.nair@gmail.com>
2025-05-14 18:49:25 -0400

    commit

---
+++ Typecasting.py
@@ -0,0 +1,5 @@
+a = 249
+b = 14.5209305
+print("Converting float to int:", int(b))
+print("Converting int to string:", str(a), type(str(a)))
+print("Converting float to string:", str(b), type(str(b)))
